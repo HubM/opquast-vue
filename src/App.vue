379355e683bf4db056daa4a2f4b1f326031af03b
Hubert Moncenis <hubmoncenis@gmail.com>
2018-06-15 15:20:32 +0200

    HOME > Adding first section

--- src/App.vue
+++ src/App.vue
@@ -6,7 +6,6 @@
 </template>
 
 <script>
-import Button from './components/Button';
 import Header from './components/header/Header';
 
 export default {
@@ -23,16 +22,4 @@ export default {
 
 <style lang="scss">
   @import '~styles';
-.slide-fade-enter-active {
-  transition: all .3s ease;
-}
-.slide-fade-leave-active {
-  transition: all .8s cubic-bezier(1.0, 0.5, 0.8, 1.0);
-}
-.slide-fade-enter, .slide-fade-leave-to
-/* .slide-fade-leave-active below version 2.1.8 */ {
-  transform: translateX(10px);
-  opacity: 0;
-}
-
 </style>
--- src/components/layouts/Home.vue
+++ src/components/layouts/Home.vue
@@ -1,19 +1,29 @@
 <template>
-  <h1>Homepage</h1>
+  <section>
+    <article class="columns certification-opquast">
+      <div class="column">
+        <h3 class="color-scooter">certification Opquast</h3>
+        <h1>La certification <br>
+          des professionnels du web
+        </h1>
+        <p>Démontrez votre crédibilité et vos compétences. Obtenez votre certification reconnue par l'État : Maîtrise de la qualité en projet web.</p>
+        <Link class="btn btn-primary" path="/certification" message="démarrer" />
+      </div>
+      <img src="~assets/homepage/illustration_1.svg" alt="Illustration qui représente les bonnes pratiques web">
+    </article>
+    <article></article>
+    <article></article>
+  </section>
 </template>
 
 <script>
-import Header from '../header/Header';
+import Link from '../elements/Link';
 
 export default {
   name: 'home',
   components: {
-    Header
+    Link
   }
 }
 </script>
-
-<style lang="scss">
-  @import '~styles';
-</style>
 
