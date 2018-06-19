e2ade0cd1402a346fda00ac119aef1bd46ece7da
Hubert Moncenis <hubmoncenis@gmail.com>
2018-06-19 10:22:07 +0200

    CHECKLIST > Icons and active function

--- src/components/pages/Checklist/FilterChecklists.vue
+++ src/components/pages/Checklist/FilterChecklists.vue
@@ -1,7 +1,7 @@
 <template>
   <ul class="contained category-list">
-    <li v-for="(category,index) in checklists.category" :key="index">
-      <div class="category-list__content">
+    <li v-for="(category,index) in checklists.category" :key="index" @click="activeFilter(index)">
+      <div class="category-list__content" :class="{ active: active_el == index }">
         <div v-if='category.icon' v-html='category.icon' class="icon-category"></div>
         <p>{{ category.title }}</p>
       </div>
@@ -15,7 +15,13 @@ export default {
   props: ['checklists'],
   data() {
     return {
-      checklists: checklists
+      isActive: false,
+      active_el: 0,
+    }
+  },
+  methods: {
+    activeFilter(el) {
+      this.active_el = el;
     }
   }
 }
--- src/datas/checklists.js
+++ src/datas/checklists.js
@@ -1,17 +1,17 @@
 export default {
   category: {
-    // 0: {
-    //   title: 'Qualité web',
-    //   // icon: 'src/assets/checklists/icn/test-icon.svg'
-    // },
+    0: {
+      title: 'Qualité web',
+      icon: `<span class="icon-qualite-web"><span class="path1"></span><span class="path2"></span><span class="path3"></span></span>`
+    },
     1: {
       title: 'Web mobile',
       icon: `<span class="icon-mobile"><span class="path1"></span><span class="path2"></span></span>`
     },
-    // 2: {
-    //   title: 'SEO',
-    //   // icon: 'src/assets/checklists/icn/seo.svg'
-    // },
+    2: {
+      title: 'SEO',
+      icon: `<span class="icon-search"><span class="path1"></span><span class="path2"></span></span>`
+    },
     3: {
       title: 'Performance',
       icon: `<span class="icon-repair"><span class="path1"></span><span class="path2"></span></span>`
@@ -24,22 +24,20 @@ export default {
       title: 'Opquast website',
       icon: `<span class= "icon-web"><span class="path1"></span><span class="path2"></span><span class="path3"></span><span class="path4"></span><span class="path5"></span><span class="path6"></span><span class="path7"></span><span class="path8"></span><span class="path9"></span><span class="path10"></span></span>`
     },
-    // 6: {
-    //   title: 'Accesibilité premier pas',
-    //   // icon: 'src/assets/checklists/icn/accessible--1.svg'
-    // },
-    // 7: {
-    //   title: 'Accesibilité second pas',
-    //   // icon: 'src/assets/checklists/icn/accessible--2.svg'
-    // },
+    6: {
+      title: 'Accesibilité premier pas',
+      icon: `<span class="icon-accessible--1"></span>`
+    },
+    7: {
+      title: 'Accesibilité second pas',
+      icon: `<span class="icon-accessible--2"></span>`
+    },
     8: {
       title: 'Green it',
-      // icon: 'src/assets/checklists/icn/green-it.svg'
       icon: `<span class="icon-plant"><span class="path1"></span><span class="path2"></span><span class="path3"></span><span class="path4"></span><span class="path5"></span><span class="path6"></span><span class="path7"></span></span>`
     },
     9: {
       title: 'Open data',
-      // icon: 'src/assets/checklists/icn/open-data.svg'
       icon: `<span class="icon-data"></span>`
     },
   },
