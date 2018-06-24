f91848646e03b1f04c0ad11018298315432e78b7
Hubert Moncenis <hubmoncenis@gmail.com>
2018-06-24 02:28:11 +0200

    CHECKLIST > fix pb h1

--- src/components/header/MainItem.vue
+++ src/components/header/MainItem.vue
@@ -2,9 +2,10 @@
   <li @mouseover="() => this.isHover = true" @mouseout="() => this.isHover = false">
     <p class="main-items__title">
       {{ item.name }}
-      <svg xmlns="http://www.w3.org/2000/svg" width="11" height="7" viewBox="0 0 11 7">
+      <span class="icon-arrow-menu"></span>
+      <!-- <svg xmlns="http://www.w3.org/2000/svg" width="11" height="7" viewBox="0 0 11 7">
         <path fill="#03061A" fill-rule="nonzero" d="M4.793 5.5l-4-4L1.5.793l4 4 4-4 .707.707L5.5 6.207 4.793 5.5z"/>
-      </svg>
+      </svg> -->
     </p>
     <ul class="main-items__sublinks" :class="[ { active: isHover }, item.class ]">
       <subItem v-for="(link,key) in item.subLinks" :key="key" :subItem="link" :color="item.color" :lightenColor="item.lightenColor" />
